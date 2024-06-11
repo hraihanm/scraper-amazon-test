ff999fe96e3db3e349e16785ad9af41b46593691
hraihanm <raihanmuhamad@gmail.com>
2024-06-12 00:58:40 +0700

    updated header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -6,7 +6,7 @@ CSV.foreach("./seeder/asins.csv", headers: true) do |row|
     pages << {
       page_type: "products",
       method: "GET",
-      headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+      headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 OPR/110.0.0.0"},
       url: url,
       vars: {
         url: url,
